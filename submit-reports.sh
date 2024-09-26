6058321684ac71e51c1f029d491c7d730e7b6097
Motko <77466238+Motko222@users.noreply.github.com>
2024-09-26 18:43:47 +0200

    Update and rename submit-report.sh to submit-reports.sh

---
+++ submit-reports.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+[ -z $INFLUX_HOST ] && exit
+
+for i in ~/logs/report-*
+do
+   
+   tag_count=$(cat $i | jq '.tags | length')
+   field_count=$(cat $i | jq '.fields | length')
+   
+   data=$(cat $i | jq -r '.measurement')","
+   
+   for (( i=0; i<$tag_count; i++ ))
+   do
+    key=$(cat $i | jq .tags | jq -r keys[$i])
+    value=$(cat $i | jq .tags | jq -r --arg a $key '.[$a]')
+    data=$data$key"="$value
+    [ $i -lt $(( tag_count - 1 )) ] && data=$data"," || data=$data" "
+   done
+   
+   for (( i=0; i<$field_count; i++ ))
+   do
+    key=$(cat $i | jq .fields | jq -r keys[$i])
+    value=$(cat $i | jq .fields | jq -r --arg a $key '.[$a]')
+    data=$data$key"=\""$value"\""
+    [ $i -lt $(( field_count - 1 )) ] && data=$data"," || data=$data" "$(date +%s%N)
+   done
+   
+   if [ ! -z $INFLUX_HOST ]
+   then
+    curl --request POST \
+    "$INFLUX_HOST/api/v2/write?org=$INFLUX_ORG&bucket=$INFLUX_BUCKET&precision=ns" \
+     --header "Authorization: Token $INFLUX_TOKEN" \
+     --header "Content-Type: text/plain; charset=utf-8" \
+     --header "Accept: application/i" \
+     --data-binary "$data"
+   fi
+
+done
