84dea3d4c365a23ad9bb33962adeb844035870c8
Motko <77466238+Motko222@users.noreply.github.com>
2024-11-22 21:35:03 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -6,7 +6,7 @@ cd $path
 while IFS= read -r line
 do
      backup_path=$(echo $line | awk '{print $2}')
-     [ -d $backup_path ] || mkdir $backup_path
+     [ -d $backup_path ] || mkdir -p $backup_path
      cp -r $line
      echo $line
 done < backup
