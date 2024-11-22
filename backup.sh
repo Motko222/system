1cd6c0d2dc03119ecfecbaffd88fab8ca5c3d1e8
Motko <77466238+Motko222@users.noreply.github.com>
2024-11-22 21:25:54 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -5,9 +5,6 @@ cd $path
 
 while IFS= read -r line1
 do
-  while IFS= read -r line2
-   do
-     cp -r $line1 $line2
-     echo $line1   >   $line2
-   done < backup-path
-done < backup-list
+     cp -r $line1
+     echo $line1
+done < backup
